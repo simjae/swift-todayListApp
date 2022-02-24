abb034c3bd412291782274b0a5dab61c0cf3cb5e
simjae <sjhwo9370@gmail.com>
2022-02-24 14:30:47 +0900

    Initial Commit

---
+++ todayListApp/todayListAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  todayListAppApp.swift
+//  todayListApp
+//
+//  Created by MACBOOK PRO on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct todayListAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
